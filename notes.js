3dcc4f1f09242968e6615b344efd678eb3e4c00d
kolafas <aladekolawole15@yahoo.com>
2016-08-18 12:23:06 +0100

    notes app including create function

--- notes.js
+++ notes.js
@@ -1,4 +1,18 @@
-var notesApplication = function(author) {
-	this.list = [];
-	this.author = author;
+module.exports = {
+	notesApplication:function(author) {
+		this.list = [];
+		this.author = author;
+
+		/**
+		 * create function
+		 * @param note_content
+		 */
+
+		this.create = function(note_content){
+			this.list.push(note_content);
+			return this.list;
+		}
+
+
+	}
 };
